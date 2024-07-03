2c129932af3303979adb8166fbea4c662cba76fa
Nabil Mahel <nabilmhl-dev@outlook.com>
2024-07-03 22:13:18 +0200

    letsgo

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,14 @@ const app = express();
 
 app.use(express.json());
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://billythekid:nabilcanal91@cluster0.osfotbp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
