66f86f08d9cffc5158171d2d3590277ab86bdae2
Nabil Mahel <nabilmhl-dev@outlook.com>
2024-07-03 21:22:16 +0200

    onavance

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,22 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+
+  app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Objet créé !'
+    });
+  });
+
 
 app.use('/api/stuff', (req, res, next) => {
     const stuff = [
